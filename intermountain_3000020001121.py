a594f20accf4f0983673fde334d954a4db74ef13
Jonas Krueger <Jonas.Krueger@symphonyretailai.com>
2020-08-08 18:35:25 -0700

    sd

--- intermountain_3000020001121.py
+++ intermountain_3000020001121.py
@@ -43,7 +43,7 @@ new_cluster2 = {
     'spark_version': '7.0.x-scala2.12',
     'node_type_id': 'Standard_D32s_v3',
     'driver_node_type_id':'Standard_DS13_v2',
-    'num_workers': 25
+    'num_workers': 20
 }
 
 #First notebook parameter
